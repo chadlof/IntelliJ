59444dcba413fa05c5a6195bf719a9033385d711
chadlof <chadlof@gmail.com>
2016-12-19 19:42:07 -0600

    Add files via upload

---
+++ intelliJ/MoviePick/src/MoviePerson.java
@@ -0,0 +1,16 @@
+/**
+ * Created by valjesc on 12/19/2016.
+ */
+public class MoviePerson {
+    private String name;
+
+    public void setName(String Name){
+        Name = name;
+        return;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
